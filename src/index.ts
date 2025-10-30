62f5227241b8cdb24e0c4588ce37397e0b66b4c9
Oleksii <kespa71for@gmail.com>
2025-10-30 10:36:42 +0100

    feat(components): print count books in title

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { createBookRow } from "./components/createBookRow.js";
 import { getBooks } from "./api/books.js";
 
 const tbody = document.querySelector("tbody")!;
+const title = document.querySelector("h2") as HTMLElement;
 
 async function renderBooks() {
     try {
         const books = await getBooks();
+        title.innerText = ` ${books.length} Books displayed`
         books.map(book => tbody.appendChild(createBookRow(book)));
     } catch (err) {
         console.error(err);
